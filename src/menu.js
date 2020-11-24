8f2786b55aa6944c6069b422954523186905b4df
Holy-Person <holyochi@gmail.com>
2020-11-24 05:21:12 +0100

    Preview Changelog And Upcoming + Removal Of Unused Code

--- src/menu.js
+++ src/menu.js
@@ -136,7 +136,6 @@ function switchDetailContainer() {
 	for (var i = 0; i < x.length; i++) {
 	  x[i].style.display = `none`;
 	}
-	console.log(destination);
 	document.getElementById(`${destination}DC`).style.display = "block";
 }
 /*END MAINMENU*/
@@ -144,61 +143,10 @@ function switchDetailContainer() {
 
 
 /*
-Mousetrap.bind('esc', () => {
-	//Open confirmation window
-	//window.close();
-	//console.log('%c Loaded', 'color:green; font-size: 1.5em');
-})
-
-window.onload = startUp;
-function startUp() {
-	getModules();
-	fillChangelog();
-}
-
 function openModule() {
 	var element  		= event.target;
 	var destination = element.id;
 	ipcRenderer.send('changePage', `${destination}`);
-}
-
-var ModuleButton = function (mType, mName) {
-  this.defaultModule =
-  `<div class="moduleButton ${mType}Style" id="${mName}" onclick="openModule()">`+
-	`${mName}`+
-  `</div>`;
-	this.previewModule =
-  `<div class="moduleButton ${mType}Style" id="${mName}">`+
-	`${mName}`+
-  `</div>`;
-}
-
-function getModules() {
-  var filePath = `${__dirname}/Modules/`;
-
-  Fs.readdir(filePath, function(err, modules) {
-    modules.forEach(function (module) {
-      if(!module.startsWith(`off_`)) {
-
-        Fs.readFile((`${__dirname}/Modules/${module}/moduleinfo.json`), 'utf8', (err, moduleJsonString) => {
-          var infoObject = JSON.parse(moduleJsonString);
-          var moduleType = infoObject.module.type;
-					var moduleName = infoObject.module.name;
-					
-					var listModule = new ModuleButton(moduleType, moduleName);
-					
-					switch (moduleType) {
-						case "installed": document.getElementById("InstalledList").innerHTML = document.getElementById("InstalledList").innerHTML + listModule.defaultModule;
-						break;
-						case "core": document.getElementById("DefaultList").innerHTML = document.getElementById("DefaultList").innerHTML + listModule.defaultModule;
-						break;
-						case "preview": document.getElementById("DownloadList").innerHTML = document.getElementById("DownloadList").innerHTML + listModule.previewModule;
-						break;
-					}
-        });
-      }
-    });
-  });
 }
 
 async function fillChangelog() {
