5945b7524bdf96bcd059a50a8079e17497cf1df8
Holy-Person <44945796+Holy-Person@users.noreply.github.com>
2021-02-11 04:28:04 +0100

    Devplayground screen load logic

--- src/Modules/DevPlayground/index.js
+++ src/Modules/DevPlayground/index.js
@@ -1,14 +1,48 @@
+/*START SETUP*/
 const Mousetrap = require('mousetrap');
 const { ipcRenderer } = require('electron');
 const MultyTool = require(__dirname+'/../../globalFunction.js');
 
 Mousetrap.bind('esc', () => {
-	ipcRenderer.send('changePage', 'menu');
+	goBack();
 });
 Mousetrap.bind(['command+r', 'ctrl+r', 'f5'], () => {
 	window.location.reload();
 });
+function goBack() {
+	ipcRenderer.send('changePage', 'menu');
+}
+/*END SETUP*/
 
 function test() {
 	MultyTool.loadUserTheme();
 }
+
+/*START LOAD*/
+function loadButtons() {
+	var screens = document.querySelectorAll('.contentScreen');
+	console.log(screens);
+	screens.forEach(function (screen) {
+		var screenID = screen.id;
+		screenID = screenID
+			.split('cs_').join('')
+		  .split('-').join(' ');
+
+		var sb_content = new SB_Content(screenID);
+		document.getElementById('sidebarList').innerHTML += sb_content.button;
+	});
+}
+
+const SB_Content = function (buttonName) {
+  this.button =
+  `<div class="button" onclick="switchCotentScreen()">`+
+		`${buttonName}`+
+  `</div>`;
+}
+/*END LOAD*/
+
+/*START CS LOGIC*/
+function switchCotentScreen() {
+	console.log(event.target.innerHTML);
+}
+/*END CS LOGIC*/
