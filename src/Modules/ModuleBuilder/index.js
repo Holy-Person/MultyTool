92a87253d50956596956f560d53f569b057a9e6f
Holy-Person <44945796+Holy-Person@users.noreply.github.com>
2021-02-26 09:30:55 +0100

    ModuleBuilder version 2.0.0 layout setup and colors

--- src/Modules/ModuleBuilder/index.js
+++ src/Modules/ModuleBuilder/index.js
@@ -1,34 +1,72 @@
+/*START SETUP*/
 const Mousetrap = require('mousetrap');
+const { ipcRenderer } = require('electron');
 const MultyTool = require(__dirname+'/../../globalFunction.js');
 
 Mousetrap.bind('esc', () => {
-	MultyTool.goToModule('menu');
+	MultyTool.app.goToModule('menu');
 });
 Mousetrap.bind(['command+r', 'ctrl+r', 'f5'], () => {
 	window.location.reload();
 });
+/*END SETUP*/
 
-var currentType = 'main';
-function selectOption(num) {
-	var x = document.getElementsByClassName("optionCard");
-	var y = document.getElementsByClassName("optionContainer");
-	for (var i = 0; i < x.length; i++) {
-	  x[i].style.display = `none`;
-	}
-	y[num].style.display = `block`;
-	currentType = 'option';
+/*START LOAD*/
+function startUp() {
+	loadButtons();
+	loadNodes();
 }
 
-function goBack() {
-	if(currentType == 'option') {
-		var x = document.getElementsByClassName("optionCard");
-		var y = document.getElementsByClassName("optionContainer");
-		for (var i = 0; i < y.length; i++) {
-		  y[i].style.display = `none`;
-			x[i].style.display = `block`;
+function loadButtons() {
+	var screens = document.querySelectorAll('.contentItem');
+	screens.forEach(function (screen) {
+		var screenID = screen.id;
+		screenID = screenID
+			.split('cs_').join('')
+			.split('ch_').join('')
+		  .split('-').join(' ');
+
+		if (screen.classList.contains('contentScreen') ) {
+			screen.innerHTML = `<h1>${screenID}</h1><hr>` + screen.innerHTML;
+			document.getElementById('sidebarList').innerHTML += SB_Content.button(screenID);
+		} else if (screen.classList.contains('contentHeader') ) {
+			document.getElementById('sidebarList').innerHTML += SB_Content.header(screenID);
 		}
-		currentType = 'main';
-	} else {
-		ipcRenderer.send('changePage', 'menu');
-	}
+	});
+}
+
+var SB_Content = {
+	headerTop: `<div class="header">`,
+  buttonTop: `<div class="button" onclick="switchCotentScreen()">`,
+	button: function(title) {
+    return this.buttonTop + `${title}</div>`;
+  },
+	header: function(title) {
+    return this.headerTop + `${title}</div><hr>`;
+  }
+}
+/*END LOAD*/
+
+/*START CS LOGIC*/
+var activeButtonNode;
+var activeScreenNode;
+function loadNodes() {
+	activeScreenNode = document.getElementById('cs_About');
+}
+// TODO: make onload function that inits activescreennode
+function switchCotentScreen() {
+	if(activeButtonNode) { activeButtonNode.classList.remove('buttonActive'); }
+	activeButtonNode = event.target;
+	activeButtonNode.classList.add('buttonActive');
+
+	var targetScreen = activeButtonNode.innerHTML.split(' ').join('-');
+
+	if(activeScreenNode) { activeScreenNode.style.display = `none`; }
+	activeScreenNode = document.getElementById(`cs_${targetScreen}`);
+	activeScreenNode.style.display = "block";
+}
+/*END CS LOGIC*/
+
+function notificationTest(type) {
+	MultyTool.app.sendToast(type, 'Notification test.');
 }
