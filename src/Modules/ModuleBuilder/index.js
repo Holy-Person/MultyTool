25ac8ac39042656a8fb445f77d00be9f64317e7d
Holy-Person <44945796+Holy-Person@users.noreply.github.com>
2021-03-01 13:27:37 +0100

    ModuleBuilder design changes and requirement list

--- src/Modules/ModuleBuilder/index.js
+++ src/Modules/ModuleBuilder/index.js
@@ -1,6 +1,5 @@
 /*START SETUP*/
 const Mousetrap = require('mousetrap');
-const { ipcRenderer } = require('electron');
 const MultyTool = require(__dirname+'/../../globalFunction.js');
 
 Mousetrap.bind('esc', () => {
